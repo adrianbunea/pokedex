7507b23078bcc3ba27abb2db918039641a058940
adrianbunea <adrian_bunea28@yahoo.com>
2020-12-23 20:23:51 +0200

    Fix warnings

--- src/App.js
+++ src/App.js
@@ -5,15 +5,21 @@ function App() {
   const cache = useContext(CacheContext);
   const [ pokemons, setPokemons ] = useState(undefined);
 
-  useEffect(async () => {
-    getPokemon(cache).then((pokemons) => {
+  useEffect(() => {
+    async function getPokemon(cache) {
+      const endpointUrl = 'https://pokeapi.co/api/v2/pokemon/';
+      await cache.add(endpointUrl);
+      const pokemonResponse = await cache.match(endpointUrl);
+      const pokemons = await pokemonResponse.json();
       setPokemons(pokemons);
-    });
+    }
+
+    getPokemon(cache);
   }, []);
 
   const pokemonList = pokemons?.results.map((pokemon) => {
     return (
-      <li>{pokemon.name}</li>
+      <li key={pokemon.url}>{pokemon.name}</li>
     )
   });
 
@@ -22,13 +28,6 @@ function App() {
       {pokemonList}
     </ul>
   );
-}
-
-async function getPokemon(cache) {
-  const endpointUrl = 'https://pokeapi.co/api/v2/pokemon/';
-  await cache.add(endpointUrl);
-  const pokemonResponse = await cache.match(endpointUrl);
-  return pokemonResponse.json();
 }
 
 export default App;
